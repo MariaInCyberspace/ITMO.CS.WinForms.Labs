cde3e1635243f1a217adcd3ea12ba8d44a93166b
Maria Spiridonova <miaiv@icloud.com>
2021-06-17 06:12:09 +0300

    Ex.3 of Lab 1

---
+++ Lab1/WindowsFormsApp_Triangle/WindowsFormsApp_Triangle/nForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Triangle
+{
+    public partial class nForm : WindowsFormsApp_Triangle.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
