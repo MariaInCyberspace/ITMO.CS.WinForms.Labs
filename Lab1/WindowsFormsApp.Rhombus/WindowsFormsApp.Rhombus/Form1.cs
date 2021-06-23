bd326bc0ee3604b1370f942196cb13edbe5fcbf8
Maria Spiridonova <miaiv@icloud.com>
2021-06-23 04:25:59 +0300

    Ex. 5.2 of Lab 1

---
+++ Lab1/WindowsFormsApp.Rhombus/WindowsFormsApp.Rhombus/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp.Rhombus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(this.Width/2, 0), 
+                new Point(this.Width, this.Height/2), 
+                new Point(this.Width/2, this.Height), 
+                new Point(0, this.Height/2)});
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+    }
+}
